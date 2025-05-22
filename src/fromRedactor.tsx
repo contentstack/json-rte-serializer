ae88512c451e037ef4dabf932b8f7aeb4e0aaf34
Shreya Kamble <shreya.kamble@contentstack.com>
2025-05-22 12:20:15 +0530

    fix: remove invisible psace added in json text in case of nbsp

--- src/fromRedactor.tsx
+++ src/fromRedactor.tsx
@@ -206,6 +206,9 @@ export const fromRedactor = (el: any, options?:IHtmlToJsonOptions) : IAnyObject 
     if (el.textContent === '\n') {
       return null
     }
+    if (options?.addNbspForEmptyBlocks && el.textContent.trim() === '') {
+      return { text: '' }
+    }
     if (el.parentNode.nodeName === 'SPAN') {
       let attrs = { style: {} }
       const metadata = {}
@@ -297,6 +300,7 @@ export const fromRedactor = (el: any, options?:IHtmlToJsonOptions) : IAnyObject 
   let children: any = flatten(Array.from(parent.childNodes).map((child) => fromRedactor(child, options)))
   children = children.filter((child: any) => child !== null)
   children = traverseChildAndWarpChild(children, options?.allowNonStandardTags)
+
   if (children.length === 0) {
     children = [{ text: '' }]
   }
